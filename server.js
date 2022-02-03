7004aafb0b97b2fa7a3af31160ab79e6bdaf1971
Mark Fishman <gibor1122@gmail.com>
2022-02-03 14:12:51 +0200

    static folder added

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ const http=require('http');
 
 
 // Express App Configuration:
-// app.use(express.static('public')) 
+app.use(express.static('public')) 
 
 
 const port = process.env.PORT || 5000
 app.listen(port, () => {
     console.log(`Server ready at http://localhost:${port}`)
 })
-app.get('*', function (req, res) {
-    res.sendFile('index.html');
-  });
+// const path=require('./public')
+// app.get('*', function (req, res) {
+//     res.sendFile(path);
+//   });
