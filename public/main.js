95b0fdaf4f89cf66d63d8c834183386c03034cc0
Mark Fishman <gibor1122@gmail.com>
2022-02-05 17:32:50 +0200

    radio buttons fixed

--- public/main.js
+++ public/main.js
@@ -12,10 +12,27 @@ var form = document.getElementById('sheetdb-form');
         document.getElementById("email").value='';
         document.getElementById("phone").value='';
         var userMsg=document.querySelector('.user-msg');
+        var radioButtons = document.querySelectorAll('input[type=radio]');
+        radioButtons.forEach(radioBtn=>{
+        radioBtn.checked=false;
+        });
         userMsg.style.opacity=1;
         setTimeout(()=>{
             userMsg.style.opacity=0;
         },5000)
-        
     });
   });
+
+const singleButton=document.querySelector('input[type=radio][id=single]');
+const familyButton=document.querySelector('input[type=radio][id=family]');
+function checkAndUncheck(type){
+    if(type==='family'){
+        console.log('family');
+        singleButton.checked=false;
+        familyButton.checked=true;
+    }else if(type==='single'){
+        console.log('single');
+        singleButton.checked=true;
+        familyButton.checked=false;
+    }
+}
